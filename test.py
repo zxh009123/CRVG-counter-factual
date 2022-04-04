fix bugs for numerical stability

test.py
@@ -15,7 +15,8 @@ import numpy as np
 import argparse
 import json
 import scipy.io as sio
-from utils.utils import ReadConfig
+from utils.utils import ReadConfig, ValidateAll, validatenp
+
 from models.SAFA_TR import SAFA_TR
 from models.BAP import SCN_ResNet
 from models.SAFA_vgg import SAFA_vgg
@@ -115,10 +116,10 @@ if __name__ == "__main__":
     
     if opt.dataset == 'CVACT':
         data_path = os.path.join(opt.data_dir, 'CVACT')
-        validateloader = DataLoader(ActDataset(data_dir = data_path, transforms_sat=transforms_sat,transforms_grd=transforms_street, is_polar=polar_transformation, mode='val'), batch_size=batch_size, shuffle=True, num_workers=8)
+        validateloader = DataLoader(ActDataset(data_dir = data_path, transforms_sat=transforms_sat,transforms_grd=transforms_street, is_polar=polar_transformation, mode='val'), batch_size=batch_size, shuffle=False, num_workers=8)
     if opt.dataset == 'CVUSA':
         data_path = os.path.join(opt.data_dir, 'CVUSA', 'dataset')
-        validateloader = DataLoader(ImageDataset(data_dir = opt.data_dir, transforms_street=transforms_street,transforms_sat=transforms_sat, mode="val", is_polar=polar_transformation), batch_size=batch_size, shuffle=True, num_workers=8)
+        validateloader = DataLoader(ImageDataset(data_dir = data_path, transforms_street=transforms_street,transforms_sat=transforms_sat, mode="val", is_polar=polar_transformation), batch_size=batch_size, shuffle=False, num_workers=8)
 
     if opt.model == "SAFA_vgg":
         model = SAFA_vgg(safa_heads = number_SAFA_heads, is_polar=polar_transformation)
@@ -140,8 +141,12 @@ if __name__ == "__main__":
 
     print("start testing...")
 
-    valSateFeatures = None
-    valStreetFeature = None
+    # valSateFeatures = None
+    # valStreetFeature = None
+
+    sat_global_descriptor = np.zeros([8884, 4096])
+    grd_global_descriptor = np.zeros([8884, 4096])
+    val_i = 0
 
     model.eval()
     with torch.no_grad():
@@ -156,18 +161,23 @@ if __name__ == "__main__":
             # grd_global = F.normalize(grd_global)
 
             #stack features to the container
-            if valSateFeatures == None:
-                valSateFeatures = sat_global.detach()
-            else:
-                valSateFeatures = torch.cat((valSateFeatures, sat_global.detach()), dim=0)
+            # if valSateFeatures == None:
+            #     valSateFeatures = sat_global.detach()
+            # else:
+            #     valSateFeatures = torch.cat((valSateFeatures, sat_global.detach()), dim=0)
 
-            if valStreetFeature == None:
-                valStreetFeature = grd_global.detach()
-            else:
-                valStreetFeature = torch.cat((valStreetFeature, grd_global.detach()), dim=0)
+            # if valStreetFeature == None:
+            #     valStreetFeature = grd_global.detach()
+            # else:
+            #     valStreetFeature = torch.cat((valStreetFeature, grd_global.detach()), dim=0)
 
+            sat_global_descriptor[val_i: val_i + sat_global.shape[0], :] = sat_global.detach().cpu().numpy()
+            grd_global_descriptor[val_i: val_i + grd_global.shape[0], :] = grd_global.detach().cpu().numpy()
+
+            val_i += sat_global.shape[0]
     if opt.validate:
-        valAcc = ValidateAll(valStreetFeature, valSateFeatures)
+        # valAcc = ValidateAll(valStreetFeature, valSateFeatures)
+        valAcc = validatenp(sat_global_descriptor, grd_global_descriptor)
         print(f"-----------validation result---------------")
         try:
             #print epoch loss

train.py
@@ -26,7 +26,7 @@ from models.TOPK_SAFA import TK_SAFA
 
 from utils.utils import WarmUpGamma, LambdaLR, softMarginTripletLoss,\
      CFLoss, save_model, ValidateAll, WarmupCosineSchedule,\
-     ReadConfig, IntraLoss
+     ReadConfig, IntraLoss, validatenp
 
 args_do_not_overide = ['data_dir', 'verbose', 'resume_from']
 TR_BASED_MODELS = ['SAFA_TR', 'SAFA_TR50', 'TK_SAFA']
@@ -56,12 +56,12 @@ if __name__ == "__main__":
     parser.add_argument("--epochs", type=int, default=200, help="number of epochs of training")
     parser.add_argument("--batch_size", type=int, default=32, help="size of the batches")
     parser.add_argument("--lr", type=float, default=1e-4, help="learning rate")
-    parser.add_argument("--save_suffix", type=str, default='_newINTRA', help='name of the model at the end')
+    parser.add_argument("--save_suffix", type=str, default='_imp_ns', help='name of the model at the end')
     parser.add_argument("--data_dir", type=str, default='../scratch/CVUSA/dataset/', help='dir to the dataset')
     parser.add_argument("--model", type=str, help='model')
     parser.add_argument("--SAFA_heads", type=int, default=8, help='number of SAFA heads')
     parser.add_argument("--TR_heads", type=int, default=8, help='number of heads in Transformer')
-    parser.add_argument("--TR_layers", type=int, default=4, help='number of layers in Transformer')
+    parser.add_argument("--TR_layers", type=int, default=8, help='number of layers in Transformer')
     parser.add_argument("--TR_dim", type=int, default=2048, help='dim of FFD in Transformer')
     parser.add_argument("--dropout", type=float, default=0.3, help='dropout in Transformer')
     parser.add_argument("--gamma", type=float, default=10.0, help='value for gamma')
@@ -201,22 +201,26 @@ if __name__ == "__main__":
         batch_size=batch_size, shuffle=True, num_workers=8)
 
     validateloader = DataLoader(ImageDataset(data_dir = opt.data_dir, transforms_street=val_transforms_street,transforms_sat=val_transforms_sate, mode="val", is_polar=polar_transformation),\
-        batch_size=batch_size, shuffle=True, num_workers=8)
+        batch_size=batch_size, shuffle=False, num_workers=8)
 
     #To be noticed: safa_heads represent k in topk when SAFA_TR in topk mode
     #change SAFA_TR mode in uncomment and comment line 233 to 243 in models/SAFA_TR.py 
     if opt.model == "SAFA_vgg":
         model = SAFA_vgg(safa_heads = number_SAFA_heads, is_polar=polar_transformation)
+        embedding_dims = number_SAFA_heads * 512
     elif opt.model == "SAFA_TR":
         model = SAFA_TR(safa_heads=number_SAFA_heads, tr_heads=opt.TR_heads, tr_layers=opt.TR_layers, dropout = opt.dropout, d_hid=opt.TR_dim, is_polar=polar_transformation, pos=pos)
+        embedding_dims = number_SAFA_heads * 512
     elif opt.model == "SAFA_TR50":
         model = SAFA_TR50(safa_heads=number_SAFA_heads, tr_heads=opt.TR_heads, tr_layers=opt.TR_layers, dropout = opt.dropout, d_hid=opt.TR_dim, is_polar=polar_transformation, pos=pos)
+        embedding_dims = number_SAFA_heads * 512
     elif opt.model == "TK_SAFA":
         if opt.tkp == 'conv':
             TK_Pool = False
         else:
             TK_Pool = True
         model = TK_SAFA(top_k=opt.topK, tr_heads=opt.TR_heads, tr_layers=opt.TR_layers, dropout = opt.dropout, is_polar=polar_transformation, pos=pos, TK_Pool=TK_Pool)
+        embedding_dims = opt.topK * 512
     elif opt.model == "SCN_ResNet":
         model = SCN_ResNet()
     else:
@@ -302,7 +306,7 @@ if __name__ == "__main__":
                     epoch_cf_loss += CFLoss_total.item()
 
                 if opt.intra:
-                    it_loss = IntraLoss(sat_global, grd_global, loss_weight=5, hard_topk_ratio=0.4)
+                    it_loss = IntraLoss(sat_global, grd_global, loss_weight=5.0, hard_topk_ratio=0.4)
                     loss += it_loss
                     epoch_it_loss += it_loss.item()
 
@@ -344,8 +348,11 @@ if __name__ == "__main__":
         print("----------------------")
 
         # Testing phase
-        valSateFeatures = None
-        valStreetFeature = None
+        # valSateFeatures = None
+        # valStreetFeature = None
+        sat_global_descriptor = np.zeros([8884, embedding_dims])
+        grd_global_descriptor = np.zeros([8884, embedding_dims])
+        val_i = 0
 
         model.eval()
         with torch.no_grad():
@@ -355,17 +362,22 @@ if __name__ == "__main__":
 
                 sat_global, grd_global = model(sat, grd, is_cf=False)
 
-                if valSateFeatures == None:
-                    valSateFeatures = sat_global.detach()
-                else:
-                    valSateFeatures = torch.cat((valSateFeatures, sat_global.detach()), dim=0)
+                # if valSateFeatures == None:
+                #     valSateFeatures = sat_global.detach()
+                # else:
+                #     valSateFeatures = torch.cat((valSateFeatures, sat_global.detach()), dim=0)
 
-                if valStreetFeature == None:
-                    valStreetFeature = grd_global.detach()
-                else:
-                    valStreetFeature = torch.cat((valStreetFeature, grd_global.detach()), dim=0)
+                # if valStreetFeature == None:
+                #     valStreetFeature = grd_global.detach()
+                # else:
+                #     valStreetFeature = torch.cat((valStreetFeature, grd_global.detach()), dim=0)
+                sat_global_descriptor[val_i: val_i + sat_global.shape[0], :] = sat_global.detach().cpu().numpy()
+                grd_global_descriptor[val_i: val_i + grd_global.shape[0], :] = grd_global.detach().cpu().numpy()
 
-            valAcc = ValidateAll(valStreetFeature, valSateFeatures)
+                val_i += sat_global.shape[0]
+
+            # valAcc = ValidateAll(valStreetFeature, valSateFeatures)
+            valAcc = validatenp(sat_global_descriptor, grd_global_descriptor)
             logger.info("validation result")
             print(f"------------------------------------")
             try:

utils/utils.py
@@ -53,11 +53,11 @@ class LambdaLR():
         self.decay_start_epoch = decay_start_epoch
 
     def step(self, epoch):
-        return 1.0 - max(0, epoch + self.offset - self.decay_start_epoch)/(self.n_epochs - self.decay_start_epoch)
+        return 1.0 - max(0.0, epoch + self.offset - self.decay_start_epoch)/(self.n_epochs - self.decay_start_epoch)
 
 
-def softMarginTripletLoss(sate_vecs, pano_vecs, loss_weight=10, hard_topk_ratio=1.0):
-    dists = 2 - 2 * torch.matmul(sate_vecs, pano_vecs.permute(1, 0))  # Pairwise matches within batch
+def softMarginTripletLoss(sate_vecs, pano_vecs, loss_weight=10.0, hard_topk_ratio=1.0):
+    dists = 2.0 - 2.0 * torch.matmul(sate_vecs, pano_vecs.permute(1, 0))  # Pairwise matches within batch
     pos_dists = torch.diag(dists)
     N = len(pos_dists)
     diag_ids = np.arange(N)
@@ -65,45 +65,45 @@ def softMarginTripletLoss(sate_vecs, pano_vecs, loss_weight=10, hard_topk_ratio=
 
     # Match from satellite to street pano
     triplet_dist_s2p = pos_dists.unsqueeze(1) - dists
-    loss_s2p = torch.log(1 + torch.exp(loss_weight * triplet_dist_s2p))
-    loss_s2p[diag_ids, diag_ids] = 0  # Ignore diagnal losses
+    loss_s2p = torch.log(1.0 + torch.exp(loss_weight * triplet_dist_s2p))
+    loss_s2p[diag_ids, diag_ids] = 0.0  # Ignore diagnal losses
 
     if hard_topk_ratio < 1.0:  # Hard negative mining
         loss_s2p = loss_s2p.view(-1)
         loss_s2p, s2p_ids = torch.topk(loss_s2p, num_hard_triplets)
-    loss_s2p = loss_s2p.sum() / num_hard_triplets
+    loss_s2p = loss_s2p.sum() / float(num_hard_triplets)
 
     # Match from street pano to satellite
     triplet_dist_p2s = pos_dists - dists
-    loss_p2s = torch.log(1 + torch.exp(loss_weight * triplet_dist_p2s))
-    loss_p2s[diag_ids, diag_ids] = 0  # Ignore diagnal losses
+    loss_p2s = torch.log(1.0 + torch.exp(loss_weight * triplet_dist_p2s))
+    loss_p2s[diag_ids, diag_ids] = 0.0  # Ignore diagnal losses
 
     if hard_topk_ratio < 1.0:  # Hard negative mining
         loss_p2s = loss_p2s.view(-1)
         loss_p2s, p2s_ids = torch.topk(loss_p2s, num_hard_triplets)
-    loss_p2s = loss_p2s.sum() / num_hard_triplets
+    loss_p2s = loss_p2s.sum() / float(num_hard_triplets)
     # Total loss
     loss = (loss_s2p + loss_p2s) / 2.0
     return loss
 
-def IntraLoss(sate_vecs, pano_vecs, loss_weight=10, hard_topk_ratio=1.0):
+def IntraLoss(sate_vecs, pano_vecs, loss_weight=10.0, hard_topk_ratio=1.0):
     # satllite pairwise
-    dists_sat = 2 - 2 * torch.matmul(sate_vecs, sate_vecs.permute(1, 0))
+    dists_sat = 2.0 - 2.0 * torch.matmul(sate_vecs, sate_vecs.permute(1, 0))
     pos_dists_sat = torch.diag(dists_sat)
     N = len(pos_dists_sat)
     diag_ids = np.arange(N)
     num_hard_triplets = int(hard_topk_ratio * (N * (N - 1))) if hard_topk_ratio < 1.0 else N * (N - 1)
 
     triplet_dist_sat = pos_dists_sat - dists_sat
-    loss_sat = torch.log(1 + torch.exp(loss_weight * triplet_dist_sat))
-    loss_sat[diag_ids, diag_ids] = 0  # Ignore diagnal losses
+    loss_sat = torch.log(1.0 + torch.exp(loss_weight * triplet_dist_sat))
+    loss_sat[diag_ids, diag_ids] = 0.0  # Ignore diagnal losses
 
     if hard_topk_ratio < 1.0:  # Hard negative mining
         loss_sat = loss_sat.view(-1)
         loss_sat, p2s_ids = torch.topk(loss_sat, num_hard_triplets)
-    loss_sat = loss_sat.sum() / num_hard_triplets
+    loss_sat = loss_sat.sum() / float(num_hard_triplets)
 
-    dists_pano = 2 - 2 * torch.matmul(pano_vecs, pano_vecs.permute(1, 0))
+    dists_pano = 2.0 - 2.0 * torch.matmul(pano_vecs, pano_vecs.permute(1, 0))
     pos_dists_pano = torch.diag(dists_pano)
     N = len(pos_dists_pano)
     diag_ids = np.arange(N)
@@ -111,20 +111,20 @@ def IntraLoss(sate_vecs, pano_vecs, loss_weight=10, hard_topk_ratio=1.0):
 
     # pano pairwise
     triplet_dist_pano = pos_dists_pano - dists_pano
-    loss_pano = torch.log(1 + torch.exp(loss_weight * triplet_dist_pano))
-    loss_pano[diag_ids, diag_ids] = 0  # Ignore diagnal losses
+    loss_pano = torch.log(1.0 + torch.exp(loss_weight * triplet_dist_pano))
+    loss_pano[diag_ids, diag_ids] = 0.0  # Ignore diagnal losses
 
     if hard_topk_ratio < 1.0:  # Hard negative mining
         loss_pano = loss_pano.view(-1)
         loss_pano, p2s_ids = torch.topk(loss_pano, num_hard_triplets)
-    loss_pano = loss_pano.sum() / num_hard_triplets
+    loss_pano = loss_pano.sum() / float(num_hard_triplets)
 
     loss = (loss_pano + loss_sat) / 2.0
     return loss
 
 def CFLoss(vecs, hat_vecs, loss_weight=5.0):
 
-    dists = 2 * torch.matmul(vecs, hat_vecs.permute(1, 0)) - 2 
+    dists = 2.0 * torch.matmul(vecs, hat_vecs.permute(1, 0)) - 2.0
     cf_dists = torch.diag(dists)
     loss = torch.log(1.0 + torch.exp(loss_weight * cf_dists))
 
@@ -165,13 +165,32 @@ def ValidateOne(distArray, topK):
     return acc / dataAmount
 
 def ValidateAll(streetFeatures, satelliteFeatures):
-    distArray = 2 - 2 * torch.matmul(satelliteFeatures, torch.transpose(streetFeatures, 0, 1))
+    distArray = 2.0 - 2.0 * torch.matmul(satelliteFeatures, torch.transpose(streetFeatures, 0, 1))
     topOnePercent = int(distArray.shape[0] * 0.01) + 1
-    valAcc = torch.zeros((1, topOnePercent))
+    valAcc = torch.zeros((1, topOnePercent), dtype=torch.float)
     for i in range(topOnePercent):
         valAcc[0,i] = ValidateOne(distArray, i)
     
     return valAcc
+
+def validatenp(sat_global_descriptor, grd_global_descriptor):
+    dist_array = 2.0 - 2.0 * np.matmul(sat_global_descriptor, grd_global_descriptor.T)
+    
+    top1_percent = int(dist_array.shape[0] * 0.01) + 1
+    val_accuracy = np.zeros((1, top1_percent))
+    for i in range(top1_percent):
+        # val_accuracy[0, i] = validate(dist_array, i)
+        accuracy = 0.0
+        data_amount = 0.0
+        for k in range(dist_array.shape[0]):
+            gt_dist = dist_array[k,k]
+            prediction = np.sum(dist_array[:, k] < gt_dist)
+            if prediction < i:
+                accuracy += 1.0
+            data_amount += 1.0
+        accuracy /= data_amount
+        val_accuracy[0, i] = accuracy
+    return val_accuracy
 
 if __name__ == "__main__":
     a = torch.rand(10, 4096)