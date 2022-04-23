25ab2121c363e5d1c918c1b1f6b872394df588fe
Xingyu Li <xyli1905@gmail.com>
2022-04-23 16:50:13 +0800

    update local trans_utils

--- dataset/trans_utils.py
+++ dataset/trans_utils.py
@@ -1,6 +1,53 @@
 import torch
-import torch.nn.functional as F
-# from torchvision.utils import _log_api_usage_once
+
+
+def _get_image_num_channels(img) -> int:
+    if img.ndim == 2:
+        return 1
+    elif img.ndim > 2:
+        return img.shape[-3]
+
+    raise TypeError("Input ndim should be 2 or more. Got {}".format(img.ndim))
+
+def _assert_channels(img, permitted) -> None:
+    c = _get_image_num_channels(img)
+    if c not in permitted:
+        raise TypeError("Input image tensor permitted channel values are {}, but found {}".format(permitted, c))
+
+def F_t_posterize(img, bits: int):
+
+    # _assert_image_tensor(img)
+
+    if img.ndim < 3:
+        raise TypeError("Input image tensor should have at least 3 dimensions, but found {}".format(img.ndim))
+    if img.dtype != torch.uint8:
+        raise TypeError("Only torch.uint8 image tensors are supported, but found {}".format(img.dtype))
+
+    _assert_channels(img, [1, 3])
+    mask = -int(2**(8 - bits))  # JIT-friendly for: ~(2 ** (8 - bits) - 1)
+    return img & mask
+
+
+def posterize(img, bits: int):
+    """Posterize an image by reducing the number of bits for each color channel.
+
+    Args:
+        img (PIL Image or Tensor): Image to have its colors posterized.
+            If img is torch Tensor, it should be of type torch.uint8 and
+            it is expected to be in [..., 1 or 3, H, W] format, where ... means
+            it can have an arbitrary number of leading dimensions.
+            If img is PIL Image, it is expected to be in mode "L" or "RGB".
+        bits (int): The number of bits to keep for each channel (0-8).
+    Returns:
+        PIL Image or Tensor: Posterized image.
+    """
+    if not (0 <= bits <= 8):
+        raise ValueError('The number if bits should be between 0 and 8. Got {}'.format(bits))
+
+    # if not isinstance(img, torch.Tensor):
+    #     return F_pil.posterize(img, bits)
+
+    return F_t_posterize(img, bits)
 
 
 class RandomPosterize(torch.nn.Module):
@@ -29,7 +76,7 @@ class RandomPosterize(torch.nn.Module):
             PIL Image or Tensor: Randomly posterized image.
         """
         if torch.rand(1).item() < self.p:
-            return F.posterize(img, self.bits)
+            return posterize(img, self.bits)
         return img
 
     def __repr__(self) -> str:
